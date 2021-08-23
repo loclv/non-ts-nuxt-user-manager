09e6bfc3d0a92a0bf34759bbb83163e584f0a986
loclv <masamonedante@gmail.com>
2021-08-24 01:59:38 +0700

    refactor: rm unused

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,11 +42,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: ['nuxt-webfontloader'],
-  webfontloader: {
-    google: {
-      families: ['Roboto:400,700'],
-    },
-  },
+  webfontloader: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
