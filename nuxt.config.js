eb882ce335b106b4069fdc2d031d863ea05d81fe
loclv <masamonedante@gmail.com>
2021-08-24 14:32:01 +0700

    chore: add common meta content

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'User manager Nuxt without TypeScript for training',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
